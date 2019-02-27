2b857cbf526eac47f3fa21de9404ca2fa30295eb
Malcolm <malcolmivy7@gmail.com>
2019-02-26 19:10:55 -0600

    This is the best rivisions, for the final

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
-  
 import "./style.css";
-
-	 import Vue from 'vue/dist/vue.js'
+import Vue from 'vue/dist/vue.js';
 
 var app = new Vue({
-  el: '#app',
-  data: {
+	el: '#app',
+	data: {
+		pageName: "home", 
+		termlist: ["IL = He" , "JE = I", 'TU = You'],
+		cardnumber: 1
+	},
+	computed:{
+		cardcount: function(){
+			return this.termlist.length;
+		},
+		term: function(){
+			return this.termlist[this.cardnumber -1];
+		}
+	},
+	methods: {
+		scroll: function(direction) {
+			this.cardnumber += direction;
 
-  	pageName: "home"
-    
-         
-   
+	
 
-  }
+	   		if(this.cardnumber > this.cardcount ){
+	   			this.cardnumber=1;
+	   		}
+
+	   		if (this.cardnumber < 1){
+
+	   			this.cardnumber = this.cardcount;
+	   		}
+		}
+	}
 });
-
-
-
 
 window.app = app;
